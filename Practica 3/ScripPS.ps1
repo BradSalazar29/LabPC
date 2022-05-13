8ec6a3d2b03f561c8c213c84cd02140124e230ee
BradSalazar29 <101901837+BradSalazar29@users.noreply.github.com>
2022-05-13 12:53:29 -0500

    Add files via upload

---
+++ Practica 3/ScripPS.ps1
@@ -0,0 +1,8 @@
+﻿$num = Read-Host -Prompt "Dime un número"
+if($num -gt 10){
+    $num = Read-Host -Prompt "Dime un número que sea menor a 10"
+}
+
+for($i = 0; $i -le 10; $i++){ #$i+=1
+    Write-Host $i "x" $num "=" ($i*$num)
+}
